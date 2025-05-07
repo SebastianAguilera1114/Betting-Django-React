358cf6724a36b084387ba6b4ace0ee37572dcef4
SebastianAguilera1114 <sebastian.aguilera.dev@gmail.com>
2025-05-07 08:53:00 -0400

    Update Main.py

--- main.py
+++ main.py
@@ -7,17 +7,6 @@ def hash_md5_func(text):
     return hash_text
 
 
-def hash_sha1_func(text):
-    import hashlib
-    textUtf8 = text.encode("utf-8")
-    hash_object = hashlib.sha1(textUtf8)
-    hash_text = hash_object.hexdigest()
-
-    return hash_text
-
-import hashlib
-
-
 def ripemd160(data):
     return hashlib.new("ripemd160", data)
 
