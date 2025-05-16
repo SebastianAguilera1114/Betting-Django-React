29c7b87b06073171bd57b40b126cdd1fe90aeb18
SebastianAguilera1114 <sebastian.aguilera.dev@gmail.com>
2025-05-16 09:08:25 -0400

    Update mySite

---
+++ mySite/oneXbet/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from oneXbet.models import *
+
+# Register your models here.
+
+admin.site.register(League)
+admin.site.register(Club)
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(Betting)
+admin.site.register(MyAppUser)
