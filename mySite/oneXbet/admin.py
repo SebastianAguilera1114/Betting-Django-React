4d9130500c0400855ecb5f1dfa5cbb02d3ebd320
SebastianAguilera1114 <sebastian.aguilera.dev@gmail.com>
2025-05-16 09:09:12 -0400

    Update mySite

--- mySite/oneXbet/admin.py
+++ mySite/oneXbet/admin.py
@@ -5,6 +5,7 @@ from oneXbet.models import *
 # Register your models here.
 
 admin.site.register(League)
+admin.site.register(Club)
 admin.site.register(Player)
 admin.site.register(Game)
 admin.site.register(Betting)
