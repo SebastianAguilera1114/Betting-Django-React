9fb5ea535a3657c6def46b2a4e9729eee1293b94
SebastianAguilera1114 <sebastian.aguilera.dev@gmail.com>
2025-05-28 11:57:25 -0400

    Update oneXbet

--- mySite/oneXbet/urls.py
+++ mySite/oneXbet/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('leagues', football, name="football"),
     path('leagues/<slug:slug>/', league, name="league"),
     path('leagues/<slug:slug>/games/', games, name="games"),
+    path('leagues/<slug:slug>/games/<slug:gameSlug>', game, name="game"),
     path('contact/', cache_page(120)(ContactFormView.as_view()), name='contact'),
     path('profile', profileView, name='profile'),
     # path('leagues/<slug:slug>/<slug:club>', league, name="club"),
